7923a3254a2644ce51cae83130fdf86d73555369
Nashrah Naseem <54800516+nashrahnaseem@users.noreply.github.com>
2022-08-22 09:02:04 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["dist/index.html","dist/style.css", "src/style.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
